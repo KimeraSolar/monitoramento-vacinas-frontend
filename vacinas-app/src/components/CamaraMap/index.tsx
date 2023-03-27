39bc0d2a9d3d968602c96492aa978ed2cef8d3c4
Pessoal <pessoal@MacBook-Pro-de-Digibee.local>
2023-03-27 19:59:14 -0300

    :recycle: exibição das câmaras no mapa

--- vacinas-app/src/components/CamaraMap/index.tsx
+++ vacinas-app/src/components/CamaraMap/index.tsx
@@ -1,20 +1,39 @@
-import { LatLngExpression } from "leaflet";
+import { icon, LatLngExpression, point } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"; 
 import { Container } from "./style";
+import userMarker from '../../assets/user-marker.png';
+import camaraMarker from '../../assets/chamber-marker.png';
+import camaras from '../../mocks/camaras.json';
 
 function CamaraMap() {
-    const USER_POSITION: LatLngExpression = [-20.264041794374332, -40.26488991534286];
+    const USER_POSITION: LatLngExpression = [-20.251722001753862, -40.26626320636542];
+
+    const userIcon = icon({
+        iconUrl: userMarker,
+    });
+
+    const camaraIcon = icon({
+        iconUrl: camaraMarker,
+    })
 
     return (
         <Container>
-            <MapContainer center={USER_POSITION} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={USER_POSITION} zoom={14} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={USER_POSITION}>
-                    <Popup>Você está aqui</Popup>
+                <Marker position={USER_POSITION} icon={userIcon}>
+                    <Popup offset={point(25, 0)}>Você está aqui</Popup>
                 </Marker>
+                {camaras.map(camara => (
+                    <Marker 
+                        position={camara.camaraLocation as LatLngExpression}
+                        icon={camaraIcon}
+                    >
+                        <Popup offset={point(25, 0)}>Câmara {camara.camaraName}</Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </Container>
     )
