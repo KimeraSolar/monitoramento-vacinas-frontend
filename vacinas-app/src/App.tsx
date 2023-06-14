ebe099c7e2a5e5c69f7baba83c95b3de820388de
LBBassani <lorenabassani12@gmail.com>
2023-06-14 10:04:09 -0300

    :recycle: utiliza context para guardar o nome de usuario do gerente

--- vacinas-app/src/App.tsx
+++ vacinas-app/src/App.tsx
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import CamaraView, { camaraLoader } from './pages/CamaraView';
 import Dashboard from './pages/Dashboard';
 import { Container, Main } from './styles/global';
+import { useState } from 'react';
+import { GerenteContext } from './contexts/gerenteContext';
 
 function App() {
   const router = createBrowserRouter(
@@ -19,14 +21,18 @@ function App() {
       }
     ]
   )
+
+  const [username, setUsername ] = useState('g01');
+  
   return (
-    <Container>
-      <Header />
-      <Main>
-        <RouterProvider router={router}/>
-      </Main>
-    </Container>
-      
+    <GerenteContext.Provider value={{username, setUsername}}>
+      <Container>
+        <Header />
+        <Main>
+          <RouterProvider router={router}/>
+        </Main>
+      </Container>
+    </GerenteContext.Provider>
   );
 }
 
